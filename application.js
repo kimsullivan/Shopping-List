e262585f5a888745082e3847a4b77ac710d3a9d8
kimsullivan <kimberlyksull@gmail.com>
2015-03-31 08:04:14 -0500

    application changes

--- application.js
+++ application.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
 	$('.add').click(function() {
 		var listItem = $("#list-item").val();
+		event.preventDefault();
 		$('.items').append("<li class='line'><button class='move'></button><p>" +listItem+ "</p><button class='remove'></button></li>");
 	 });
+
+	
+});
+
+ $(document).on("click", ".remove", function() {
+        $(this).closest("li").fadeOut(200);
+    });
+
+
 
 });
 
